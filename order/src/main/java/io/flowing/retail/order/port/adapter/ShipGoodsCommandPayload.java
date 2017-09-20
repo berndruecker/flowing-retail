cf754054570ae42911445efd634b8e229f973601
Bernd Rücker <bernd.ruecker@camunda.com>
2017-09-20 19:37:21 +0200

    added accidentally deleted file

---
+++ order/src/main/java/io/flowing/retail/order/port/adapter/ShipGoodsCommandPayload.java
@@ -0,0 +1,46 @@
+package io.flowing.retail.order.port.adapter;
+
+public class ShipGoodsCommandPayload {
+  
+  private String refId;
+  private String pickId;
+  private String logisticsProvider;
+  private String recipientName;
+  private String recipientAddress;
+  
+  public String getPickId() {
+    return pickId;
+  }
+  public ShipGoodsCommandPayload setPickId(String pickId) {
+    this.pickId = pickId;
+    return this;
+  }
+  public String getLogisticsProvider() {
+    return logisticsProvider;
+  }
+  public ShipGoodsCommandPayload setLogisticsProvider(String logisticsProvider) {
+    this.logisticsProvider = logisticsProvider;
+    return this;
+  }
+  public String getRecipientName() {
+    return recipientName;
+  }
+  public ShipGoodsCommandPayload setRecipientName(String recipientName) {
+    this.recipientName = recipientName;
+    return this;
+  }
+  public String getRecipientAddress() {
+    return recipientAddress;
+  }
+  public ShipGoodsCommandPayload setRecipientAddress(String recipientAddress) {
+    this.recipientAddress = recipientAddress;
+    return this;
+  }
+  public String getRefId() {
+    return refId;
+  }
+  public ShipGoodsCommandPayload setRefId(String refId) {
+    this.refId = refId;
+    return this;
+  }
+}
