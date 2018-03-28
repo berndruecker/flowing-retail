d3aeb763e018f2418fc95510e842adbf9f8230e8
Bernd Rücker <bernd.ruecker@camunda.com>
2018-03-28 13:34:20 +0200

    added possibility to slow down service

--- rest/java/stripe-fake/src/main/java/io/flowing/retail/stripe/fake/StripeFakeApplication.java
+++ rest/java/stripe-fake/src/main/java/io/flowing/retail/stripe/fake/StripeFakeApplication.java
@@ -1,5 +1,7 @@
 package io.flowing.retail.stripe.fake;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,28 @@ public class StripeFakeApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(StripeFakeApplication.class, args);
+    
+    try {
+      System.out.println("Service is operating normal");
+  
+      Scanner scanner = new Scanner(System.in);
+  
+      while (true) {
+        System.out.print("[S]low, [N]ormal: ");
+        String mode = scanner.next().toUpperCase();
+        if ("S".equals(mode)) {
+          StripeFakeRestController.slow = true;
+          System.out.println("Service is now slow");
+        }
+        else if ("N".equals(mode)) {
+          StripeFakeRestController.slow = false;
+          System.out.println("Service is back to normal");
+        }
+      }
+    }
+    catch (Exception ex) {
+      // silently ignore
+    }
   }
 
 }
--- rest/java/stripe-fake/src/main/java/io/flowing/retail/stripe/fake/StripeFakeRestController.java
+++ rest/java/stripe-fake/src/main/java/io/flowing/retail/stripe/fake/StripeFakeRestController.java
@@ -2,6 +2,7 @@ package io.flowing.retail.stripe.fake;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StripeFakeRestController {    
+public class StripeFakeRestController { 
+  
+  public static boolean slow = false;
 
   public static class CreateChargeRequest {
     public int amount;
@@ -21,19 +24,20 @@ public class StripeFakeRestController {
   }
   
   @RequestMapping(path = "/charge", method = POST)
-  public CreateChargeResponse chargeCreditCard(@RequestBody CreateChargeRequest request) throws Exception {
+  public CreateChargeResponse chargeCreditCard(@RequestBody CreateChargeRequest request) throws Exception {   
     CreateChargeResponse response = new CreateChargeResponse();
     
     long waitTimeMillis = 0;
-//    long waitTimeMillis = Math.round( Math.random() * 60 * 1000 ); // up to 60 seconds  
+    if (slow) {
+      waitTimeMillis = Math.round( Math.random() * 60 * 1000 ); // up to 60 seconds
+    }
 
-    System.out.println("CHARGE ON CREDIT CARD ... (takes " + waitTimeMillis/1000 + " seconds)"); // " + request.amount + "
-    
-    Thread.sleep(waitTimeMillis);
-    
     if (Math.random() > 0.8d) {
       response.errorCode = "credit card expired";
     }
+    
+    System.out.println("Charge on credit card will take " + waitTimeMillis/1000 + " seconds");
+    Thread.sleep(waitTimeMillis);   
     
     response.transactionId = UUID.randomUUID().toString();
     return response;
