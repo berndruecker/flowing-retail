bae36ecfd8d724ce26553e5d6968afee4c2c1e0e
Bernd Rücker <bernd.ruecker@camunda.com>
2018-07-31 14:50:41 +0200

    use external task client for JS worker

--- rest/java/payment/node-customer-credit-worker/index.js
+++ rest/java/payment/node-customer-credit-worker/index.js
@@ -1,71 +1,23 @@
 var request = require('request');
+const { Client, Variables } = require("camunda-external-task-client-js");
 
-var baseUrl = process.env.ENGINE_URL || 'http://localhost:8100/rest/engine/default/';
-var workerId = "worker123";
-var topicName1 = "customer-credit";
-var topicName2 = "customer-credit-refund";
+const config = { baseUrl: "http://localhost:8100/rest/engine/default", interval: 50};
+const client = new Client(config);
 
-poll();
-
-function poll() {
-  request.post({
-    headers: {'content-type' : 'application/json'},
-    url: baseUrl + 'external-task/fetchAndLock',
-    body: `{ 
-        "workerId":"`+workerId+`",
-        "maxTasks":100,
-        "usePriority":true,
-        "topics":
-            [{
-              "topicName": "`+topicName1+`",
-              "lockDuration": 10000,
-              "variables": ["payload"]
-            }, {
-              "topicName": "`+topicName2+`",
-              "lockDuration": 10000,
-              "variables": ["payload"]
-            }]
-        }`
-    }, function (error, response, body) {
-      if (!error) {
-          var tasks = JSON.parse(body);
-          if (tasks.length==0) {
-            process.stdout.write(".");            
-          }
-          for (index = 0; index < tasks.length; ++index) {
-            execute(tasks[index]);
-          }
-          setTimeout(function() {
-               poll();
-          }, 50);
-       }     
-  });
-}
-
-function execute(externalTask) {
-  console.log('[%s] done for process instance %s', externalTask.topicName, externalTask.processInstanceId);
-  complete(externalTask);
-}
-
-function complete(externalTask) {
+client.subscribe("customer-credit", async function({ task, taskService }) {
   var remainingAmount = 0;
   if (Math.random() > 0.3) {
     remainingAmount = 15;   
   }
 
-  request.post({
-    headers: {'content-type' : 'application/json'},
-    url: baseUrl + 'external-task/' + externalTask.id + "/complete",
-    body: `{ 
-        "workerId":"`+workerId+`",
-        "variables":
-            {
-              "remainingAmount": {"value": `+remainingAmount+`, "type": "integer"}
-            }
-        }`
-    }, function (error, response, body) {
-      if (error) {
-        console.log(response);
-      }     
-  });
-}
+  const processVariables = new Variables();
+  processVariables.set("remainingAmount", remainingAmount);
+
+  console.log('[%s] done for process instance %s with remainingAmount=%s', task.topicName, task.processInstanceId, remainingAmount);
+  await taskService.complete(task, processVariables);
+});
+
+client.subscribe("customer-credit-refund", async function({ task, taskService }) {
+  console.log('[%s] done for process instance %s', task.topicName, task.processInstanceId);
+  await taskService.complete(task);
+});
