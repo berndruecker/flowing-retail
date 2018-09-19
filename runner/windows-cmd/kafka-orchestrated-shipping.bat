7029d898a6ce4fa173e526f66387e9098f187058
Bernd Rücker <bernd.ruecker@camunda.com>
2018-09-19 16:07:52 +0200

    fixed batch files

--- runner/windows-cmd/kafka-orchestrated-shipping.bat
+++ runner/windows-cmd/kafka-orchestrated-shipping.bat
@@ -1,2 +1,4 @@
+@echo off
+FOR /F "tokens=1,2 delims==" %%G IN (environment.properties) DO (set %%G=%%H)
 TITLE Shipping
-mvn package exec:java -f shipping/
+mvn package exec:java -f %flowing%\kafka\java\shipping\
