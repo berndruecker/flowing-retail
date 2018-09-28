5301b7d60539e26f73a7a654461a8a4bd730b328
Bernd Rücker <bernd.ruecker@camunda.com>
2018-09-28 18:03:01 +0200

    adjusted port of simple monitor when starting via batch, swoitched to alpha4 docker image of Zeebe (will be on DockerHub soon)

--- runner/windows-cmd/zeebe-2-simple-monitor-start.bat
+++ runner/windows-cmd/zeebe-2-simple-monitor-start.bat
@@ -4,4 +4,4 @@ FOR /F "tokens=1,2 delims==" %%G IN (environment.properties) DO (set %%G=%%H)
 TITLE Zeebe Simple Monitor
 sc start MongoDB
 
-java -jar %simplemonitor%
+java -Dserver.port=8096 -jar %simplemonitor%
