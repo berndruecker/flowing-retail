027786a86771b12f75dd9eb230ced407325038be
Bernd Rücker <bernd.ruecker@camunda.com>
2020-03-10 14:22:43 +0100

    switched to latest Camunda release version

--- kafka/java/order-camunda/src/main/java/io/flowing/retail/order/persistence/OrderRepository.java
+++ kafka/java/order-camunda/src/main/java/io/flowing/retail/order/persistence/OrderRepository.java
@@ -1,9 +1,11 @@
 package io.flowing.retail.order.persistence;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
 import io.flowing.retail.order.domain.Order;
 
+@Component
 public interface OrderRepository extends CrudRepository<Order, String> {
 
 }
