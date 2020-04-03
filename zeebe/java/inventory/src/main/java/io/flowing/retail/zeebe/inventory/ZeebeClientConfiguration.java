80995d21c132e2e10586bdcb90a6c316c991bd6b
Bernd Rücker <bernd.ruecker@camunda.com>
2020-04-03 22:02:48 +0200

    allowed for easy Camunda Cloud usage, switched to Java > 8 & newer Spring Boot version

--- zeebe/java/inventory/src/main/java/io/flowing/retail/zeebe/inventory/ZeebeClientConfiguration.java
+++ zeebe/java/inventory/src/main/java/io/flowing/retail/zeebe/inventory/ZeebeClientConfiguration.java
@@ -1,17 +1,58 @@
 package io.flowing.retail.zeebe.inventory;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.zeebe.client.ZeebeClient;
+import io.zeebe.client.ZeebeClientBuilder;
+import io.zeebe.client.impl.oauth.OAuthCredentialsProvider;
+import io.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 
 @Configuration
 public class ZeebeClientConfiguration {
-  
-  @Bean
-  public ZeebeClient zeebe() {
-    ZeebeClient zeebeClient = ZeebeClient.newClient();        
-    return zeebeClient;
-  }
+
+	@Value("${camunda.cloud.clusterId:#{null}}")
+	private String clusterId;
+
+	@Value("${camunda.cloud.baseUrl:zeebe.camunda.io}")
+	private String baseUrl;
+
+	@Value("${camunda.cloud.clientId:#{null}}")
+	private String clientId;
+
+	@Value("${camunda.cloud.clientSecret:#{null}}")
+	private String clientSecret;
+
+	@Value("${camunda.cloud.authUrl:'https://login.cloud.camunda.io/oauth/token'}")
+	private String authUrl;
+
+	@Bean
+	public ZeebeClient zeebe() {
+		if (clusterId==null) {
+			System.out.println("*** Connect to Zeebe locally (no Camunda Cloud cluster id set) ***");
+			
+			ZeebeClient client = ZeebeClient.newClientBuilder().usePlaintext().build();
+			System.out.println("*** Connected sucessfully to Zeebe locally ***");
+			return client;
+		} else {
+			System.out.println("*** Connect to Camunda Cloud (cluster id "+clusterId+") ***");
+			
+			final String broker = clusterId + "." + baseUrl + ":443";
+	
+			final OAuthCredentialsProviderBuilder c = new OAuthCredentialsProviderBuilder();
+			final OAuthCredentialsProvider cred = c.audience(clusterId + "." + baseUrl).clientId(clientId)
+					.clientSecret(clientSecret).authorizationServerUrl(authUrl).build();
+	
+			final ZeebeClientBuilder clientBuilder = ZeebeClient.newClientBuilder().brokerContactPoint(broker)
+					.credentialsProvider(cred);
+	
+			ZeebeClient client = clientBuilder.build();
+	
+			System.out.println("*** Connected sucessfully to Camunda Cloud ***");
+	
+			return client;
+		}
+	}
 
 }
