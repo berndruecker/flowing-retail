844cd0ef33f780e01e8a62c7b1116d2f3665a182
Bernd Rücker <bernd.ruecker@camunda.com>
2021-05-11 12:04:34 +0200

    cleaned up properties

--- runner/windows-cmd/zeebe-order.bat
+++ runner/windows-cmd/zeebe-order.bat
@@ -1,4 +1,4 @@
 @echo off
 FOR /F "tokens=1,2 delims==" %%G IN (environment.properties) DO (set %%G=%%H)
 TITLE Order
-mvn package exec:java -f %flowing%\zeebe\java\order\
+mvn package exec:java -f %flowing%\zeebe\java\order-fulfillment\
